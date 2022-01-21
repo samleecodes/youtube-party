8b030bce0738ac80b46b1d49342fe5ea780c8a02
niazmohsameer <niazmohsameer@gmail.com>
2022-01-20 21:48:47 -0500

    Refactor UI

--- packages/client/src/components/screens/Home.tsx
+++ packages/client/src/components/screens/Home.tsx
@@ -2,8 +2,13 @@ import { Component } from "solid-js";
 import { bridge } from "../../lib/bridge";
 
 const Home: Component = () => {
+    let unameInput: HTMLInputElement | undefined;
     let joinInput: HTMLInputElement | undefined;
     let createInput: HTMLInputElement | undefined;
+
+    function onUNameInputValueChanged(event: Event) {
+        console.log(event);
+    }
 
     function onJoinFormSubmitted(event: Event) {
         event.preventDefault();
@@ -26,9 +31,18 @@ const Home: Component = () => {
             <div class="text-3xl text-white">
                 YouTube Party <span class="text-sm">alpha</span>
             </div>
-            <div class="mb-2 text-lg text-white">
-                with &lt;3 by Sameer Niaz
-            </div>
+            <div class="mb-2 text-lg text-white">with &lt;3 by Sameer Niaz</div>
+            {/* <div class="flex flex-row items-center my-2">
+                <input
+                    onChange={e => {
+                        onUNameInputValueChanged(e);
+                    }}
+                    ref={unameInput}
+                    class="px-2 py-1 rounded-md"
+                    placeholder="Username"
+                    type="text"
+                />
+            </div> */}
             <form onSubmit={onJoinFormSubmitted} class="flex flex-row items-center my-2">
                 <input ref={joinInput} class="px-2 py-1 rounded-md" placeholder="Room ID" type="text" />
                 <button class="mx-2 px-2 py-1 rounded-md bg-white" type="submit">
--- packages/client/src/components/screens/Play.tsx
+++ packages/client/src/components/screens/Play.tsx
@@ -5,7 +5,7 @@ const Play: Component = () => {
         <div
             id={import.meta.env.VITE_PLAYER_DIV_ID}
             className="w-screen h-screen flex justify-center items-center text-white bg-black">
-            Loading player
+            Loading
         </div>
     );
 };
